79ff83910d89cedea2d31aa9ec27a4e781949314
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2024-05-29 11:15:03 +0800

    Update KProcess.h

--- app/src/main/cpp/skyline/kernel/types/KProcess.h
+++ app/src/main/cpp/skyline/kernel/types/KProcess.h
@@ -69,7 +69,11 @@ namespace skyline {
             KProcess(const DeviceState &state);
 
             ~KProcess();
-
+                
+            bool is64bit() {
+                return npdm.meta.flags.is64Bit;
+            }
+                
             /**
              * @brief Kill the main thread/all running threads in the process in a graceful manner
              * @param join Return after the main thread has joined rather than instantly
@@ -117,7 +121,7 @@ namespace skyline {
                 std::unique_lock lock(handleMutex);
 
                 std::shared_ptr<objectClass> item;
-                if constexpr (std::is_same<objectClass, KThread>())
+                if constexpr (std::is_base_of<KThread, objectClass>())
                     item = std::make_shared<objectClass>(state, constant::BaseHandleIndex + handles.size(), args...);
                 else
                     item = std::make_shared<objectClass>(state, args...);
@@ -142,17 +146,17 @@ namespace skyline {
                 std::shared_lock lock(handleMutex);
 
                 KType objectType;
-                if constexpr(std::is_same<objectClass, KThread>()) {
+                if constexpr (std::is_same<objectClass, KThread>()) {
                     constexpr KHandle threadSelf{0xFFFF8000}; // The handle used by threads to refer to themselves
                     if (handle == threadSelf)
                         return state.thread;
                     objectType = KType::KThread;
-                } else if constexpr(std::is_same<objectClass, KProcess>()) {
+                } else if constexpr (std::is_same<objectClass, KProcess>()) {
                     constexpr KHandle processSelf{0xFFFF8001}; // The handle used by threads in a process to refer to the process
                     if (handle == processSelf)
                         return state.process;
                     objectType = KType::KProcess;
-                } else if constexpr(std::is_same<objectClass, KSharedMemory>()) {
+                } else if constexpr (std::is_same<objectClass, KSharedMemory>()) {
                     objectType = KType::KSharedMemory;
                 } else if constexpr(std::is_same<objectClass, KTransferMemory>()) {
                     objectType = KType::KTransferMemory;
