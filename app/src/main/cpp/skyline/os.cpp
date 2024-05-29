f7b1efb1bae0292bd34673d170ad970e37437599
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2024-05-29 11:36:55 +0800

    Update os.cpp

--- app/src/main/cpp/skyline/os.cpp
+++ app/src/main/cpp/skyline/os.cpp
@@ -67,6 +67,9 @@ namespace skyline::kernel {
                 publisher = nacp->GetApplicationPublisher(nacp->GetFirstSupportedTitleLanguage());
             LOGINF(R"(Starting "{}" ({}) v{} by "{}")", name, nacp->GetSaveDataOwnerId(), nacp->GetApplicationVersion(), publisher);
         }
+
+        // Scheduler retrieves information from the NPDM of the process so it needs to be initialized after the process is created
+        state.scheduler = std::make_shared<kernel::Scheduler>(state);
         
         if (process->is64bit()) {
             state.nce = std::make_shared<nce::NCE>(state);
